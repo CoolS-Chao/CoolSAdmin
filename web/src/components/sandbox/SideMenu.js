08ed16392e8f4a0a996c12178b86e2f99d9b39ac
gcc <Gcc_chaochao@126.com>
2022-11-22 20:09:08 +0800

    pachage version

--- web/src/components/sandbox/SideMenu.js
+++ web/src/components/sandbox/SideMenu.js
@@ -1,38 +1,73 @@
 import React from "react";
+import "./index.css"
 import { Layout, Menu } from "antd";
 import {
-    UploadOutlined,
     UserOutlined,
-    VideoCameraOutlined,
   } from '@ant-design/icons';
 const { Sider } = Layout;
+const { SubMenu } = Menu;
+
+//模拟数据结构
+const menuList = [
+	{
+		key: "/user_manage",
+		icon: <UserOutlined />,
+		label: "用户管理",
+		children: [
+			{
+				key: "/user_manage/list",
+				icon: <UserOutlined />,
+				label: "用户列表"
+			}
+		]
+	},
+	{
+		key: "right_manage",
+		icon: <UserOutlined />,
+		label: "权限管理",
+		children: [
+			{
+				key: "/user_manage/list",
+				icon: <UserOutlined />,
+				label: "用户列表"
+			},
+		]
+	}
+]
 
 export default function SideMenu() {
-  return (
-    <Sider trigger={null} collapsible>
-      <div className="logo" />
-      <Menu
-        theme="dark"
-        mode="inline"
-        defaultSelectedKeys={['1']}
-        items={[
-          {
-            key: '1',
-            icon: <UserOutlined />,
-            label: 'nav 1',
-          },
-          {
-            key: '2',
-            icon: <VideoCameraOutlined />,
-            label: 'nav 2',
-          },
-          {
-            key: '3',
-            icon: <UploadOutlined />,
-            label: 'nav 3',
-          },
-        ]}
-      />
-    </Sider>
-  )
+	const renderMenu = (menuList) => {
+		return menuList.map(item=>{
+			if (item.children) {
+				return <SubMenu key={item.key} icon={item.icon} title={item.title}></SubMenu>
+			}
+			return <Menu.Item key={item.key} icon={item.icon}>{item.title}</Menu.Item>
+		})
+	}
+	return (
+		<Sider trigger={null} collapsible collapsed={false}>
+      		<div className="logo">博客发布管理系统</div>
+    		<Menu theme="dark" mode="inline" defaultSelectedKeys={['2']}
+    			// items={[
+          		// 	{
+				// 		key: '1',
+				// 		icon: <UserOutlined />,
+				// 		label: 'nav 1',
+          		// 	},
+          		// 	{
+				// 		key: '2',
+				// 		icon: <VideoCameraOutlined />,
+				// 		label: 'nav 2',
+				// 		children: [{ label: '子菜单项', key: 'submenu-item-1' }],
+				// 	},
+				// 	{
+				// 		key: '3',
+				// 		icon: <UploadOutlined />,
+				// 		label: 'nav 3',
+				// 	},
+        		// ]}
+				{...renderMenu(menuList)}
+    		/>
+    	</Sider>
+	)
 }
--- web/src/components/sandbox/TopHeader.js
+++ web/src/components/sandbox/TopHeader.js
@@ -1,33 +1,63 @@
 import React, { useState } from "react";
-import { Layout } from "antd";
+import { Layout, Dropdown, Menu, Space, Avatar, Image } from "antd";
 import {
-    MenuFoldOutlined,
-    MenuUnfoldOutlined,
-  } from '@ant-design/icons';
+	MenuFoldOutlined,
+	MenuUnfoldOutlined,
+	SmileOutlined
+} from '@ant-design/icons';
 
 const { Header } = Layout;
 
 export default function TopHeader() {
-  //函数式组件利用hooks设置状态.
-  const [collapsed, setCollapsed] = useState(false)
-  const changeCollapsed = () => {
-    setCollapsed(!collapsed)
-  }
-  return (
-    <Header
-      className="site-layout-background"
-      style={{
-        padding: "0 16px"
-      }}
-    >
-    {
-      collapsed?<MenuUnfoldOutlined onClick={changeCollapsed}/>
-        :<MenuFoldOutlined onClick={changeCollapsed}/>
-    }
-      {/* {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-        className: 'trigger',
-        onClick: () => setCollapsed(!collapsed),
-      })} */}
-    </Header>
-  )
+	//函数式组件利用hooks设置状态.
+	const [collapsed, setCollapsed] = useState(false)
+	const changeCollapsed = () => {
+		setCollapsed(!collapsed)
+	}
+	const menu = (
+		<Menu
+			items={[
+			{
+				key: '1',
+				label: (111),
+				icon: <SmileOutlined />
+			},
+			{
+				key: '4',
+				danger: true,
+				label: '退出登录',
+			},
+			]}
+		/>
+	);
+	return (
+    	<Header
+			className="site-layout-background"
+      		style={{
+        		padding: "0 16px"
+      		}}
+    	>
+			{
+				collapsed?<MenuUnfoldOutlined onClick={changeCollapsed}/>
+				:<MenuFoldOutlined onClick={changeCollapsed}/>
+			}
+			<div style={{float: "right"}}>
+				<span>欢迎Admin回来</span>
+				<Dropdown overlay={menu}>
+					<a onClick={(e) => e.preventDefault()}>
+						<Space>
+							<Avatar
+								src={
+									<Image
+										src="https://joeschmoe.io/api/v1/random"
+										style={{width: 32}}
+									/>
+								}
+							/>
+						</Space>
+    				</a>
+  				</Dropdown>
+			</div>
+		</Header>
+	)
 }
